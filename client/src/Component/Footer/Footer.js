d281010980abf0072070780b00f40bb6108deb2e
Tabrej Quadir <tabrezquadir6@gmail.com>
2024-07-23 21:40:37 +0530

    Footer Responsive Done

--- client/src/Component/Footer/Footer.js
+++ client/src/Component/Footer/Footer.js
@@ -3,40 +3,42 @@ import './Footer.css';
 import footer_logo from '../Asset/logo.png'
 import instagram from '../Asset/instagram_icon.png'
 import pinetrest from '../Asset/pintester_icon.png'
-import whatshapp from '../Asset/whatsapp_icon.png'
+import whatsapp from '../Asset/whatsapp_icon.png'
 
 const Footer = () => {
+    const currentDate = new Date().getFullYear();
     return (
         <div className='footer'>
-            <div className='footer-logo'>
-                <img src={footer_logo} alt='Footer-Logo' />
-                <p>SHOPPER</p>
+            <div className='footer-content'>
+                <div className='footer-logo'>
+                    <img src={footer_logo} alt='Footer-Logo' />
+                    <p>SHOPPER</p>
+                </div>
+                <ul className='footer-links'>
+                    <li>Company</li>
+                    <li>Products</li>
+                    <li>Offices</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                </ul>
+                <div className='footer-social-icons'>
+                    <div className='footer-icon-container'>
+                        <img src={instagram} alt="Instagram" />
+                    </div>
+                    <div className='footer-icon-container'>
+                        <img src={whatsapp} alt="Whatsapp" />
+                    </div>
+                    <div className='footer-icon-container'>
+                        <img src={pinetrest} alt="Pinterest" />
+                    </div>
+                </div>
             </div>
-            <ul className='footer-links'>
-                <li>Company</li>
-                <li>Products</li>
-                <li>Offices</li>
-                <li>About</li>
-                <li>Contact</li>
-            </ul>
-
-            <div className='footer-social-icon'>
-                <div className='footer-icons-container'>
-                    <img src={instagram} alt="Instagram" />
-                </div>
-                <div className='footer-icons-container'>
-                    <img src={whatshapp} alt="Whatshapp" />
-                </div>
-                <div className='footer-icons-container'>
-                    <img src={pinetrest} alt="Pinetrest" />
-                </div>
+            <div className='footer-bottom'>
+                <hr />
+                <p>Copyright © {currentDate} - All Rights Reserved</p>
             </div>
-        <div className='footer-copyright'>
-        <hr/>
-        <p> Copyright @ 2024 - All Right Reserved</p>
-        </div>
         </div>
     )
 }
 
-export default Footer
+export default Footer;
